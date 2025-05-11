f3fad5c76020f4eeb31dfe533bb55b1bf70189b7
Larysa <draglara@ukr.net>
2025-05-11 10:27:09 +0300

    updare files

---
+++ middlewares/errorHandler.js
@@ -0,0 +1,25 @@
+import { HttpError } from 'http-errors'; // Імпортуємо клас HttpError для обробки помилок HTTP з відповідними статус-кодами
+
+export const errorHandler = (err, req, res, next) => {
+  // Перевірка, чи отримали ми помилку від createHttpError
+  if (err instanceof HttpError) {
+    res.status(err.status).json({
+      status: err.status,
+      message: err.name,
+      data: err,
+    });
+    return;
+  }
+  res.status(500).json({
+    status: 500,
+    message: 'Something went wrong',
+    data: err.message,
+  });
+};
+
+// err instanceof HttpError — це перевірка в JavaScript або TypeScript, яка визначає, чи є змінна err екземпляром класу HttpError.
+
+// Що це означає?
+// instanceof — оператор, який перевіряє, чи створено об'єкт за допомогою конкретного конструктора (тобто класу).
+
+// HttpError — це клас, який зазвичай розширює стандартний клас Error і додає інформацію, специфічну для HTTP (наприклад, код статусу, повідомлення тощо).
--- server.js
+++ server.js
@@ -2,7 +2,9 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import { getEnvVar } from './utils/getEnvVar.js';
-import { getAllStudents, getStudentById } from './services/students.js';
+import studentsRouter from './routers/students.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
 const PORT = Number(getEnvVar('PORT', '3000'));
 
@@ -26,44 +28,11 @@ export const startServer = () => {
     });
   });
 
-  app.get('/students', async (req, res) => {
-    const students = await getAllStudents();
+  app.use(studentsRouter);
 
-    res.status(200).json({
-      data: students,
-    });
-  });
+  app.use(notFoundHandler);
 
-  app.get('/students/:studentId', async (req, res, next) => {
-    const { studentId } = req.params;
-    const student = await getStudentById(studentId);
-
-    // Відповідь, якщо контакт не знайдено
-    if (!student) {
-      res.status(404).json({
-        message: 'Student not found',
-      });
-      return;
-    }
-
-    // Відповідь, якщо контакт знайдено
-    res.status(200).json({
-      data: student,
-    });
-  });
-
-  app.use((req, res, next) => {
-    res.status(404).json({
-      message: 'Not found',
-    });
-  });
-
-  app.use((err, req, res, next) => {
-    res.status(500).json({
-      message: 'Something went wrong',
-      error: err.message,
-    });
-  });
+  app.use(errorHandler);
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
