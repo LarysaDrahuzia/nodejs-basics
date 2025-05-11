f934b6774e0c25b598887a5d3cf20ed794a0e8af
Larysa <draglara@ukr.net>
2025-05-11 17:08:05 +0300

    updata files

--- controllers/students.js
+++ controllers/students.js
@@ -1,4 +1,10 @@
-import { getAllStudents, getStudentById } from '../services/students.js';
+import {
+  getAllStudents,
+  getStudentById,
+  createStudent,
+  deleteStudent,
+  updateStudent,
+} from '../services/students.js';
 import createHttpError from 'http-errors'; // 1. Імпортуємо функцію з бібліотеки
 
 export const getStudentsController = async (req, res, next) => {
@@ -23,5 +29,58 @@ export const getStudentByIdController = async (req, res) => {
     status: 200,
     message: `Successfully found student with id: ${studentId}!`,
     data: student,
+  });
+};
+
+export const createStudentController = async (req, res) => {
+  const student = await createStudent(req.body);
+
+  res.status(201).json({
+    status: 201,
+    message: 'Successfully created a student!',
+    data: student,
+  });
+};
+
+export const deleteStudentController = async (req, res, next) => {
+  const { studentId } = req.params;
+  const student = await deleteStudent(studentId);
+
+  if (!student) {
+    next(createHttpError(404, 'Student not found!'));
+    return;
+  }
+  res.status(204).send();
+};
+
+export const upsertStudentController = async (req, res, next) => {
+  const { studentId } = req.params;
+  const result = await updateStudent(studentId, req.body, { upsert: true });
+
+  if (!result) {
+    next(createHttpError(404, 'Student not found'));
+    return;
+  }
+  const status = result.isNew ? 201 : 200;
+
+  res.status(status).json({
+    status,
+    message: 'Successfully upserted a student!',
+    data: result.student,
+  });
+};
+
+export const patchStudentController = async (req, res, next) => {
+  const { studentId } = req.params;
+  const result = await updateStudent(studentId, req.body);
+
+  if (!result) {
+    next(createHttpError(404, 'Student not found!'));
+    return;
+  }
+  res.json({
+    status: 200,
+    message: 'Successfully upserted a student!',
+    data: result.student,
   });
 };
--- services/students.js
+++ services/students.js
@@ -9,3 +9,47 @@ export const getStudentById = async (studentId) => {
   const student = await StudentsCollection.findById(studentId);
   return student;
 };
+
+export const createStudent = async (payload) => {
+  const student = await StudentsCollection.create(payload);
+  return student;
+};
+
+export const deleteStudent = async (studentId) => {
+  const student = await StudentsCollection.findOneAndDelete({ _id: studentId });
+  return student;
+};
+
+export const updateStudent = async (studentId, payload, options = {}) => {
+  const rawResult = StudentsCollection.findOneAndUpdate(
+    {
+      _id: studentId,
+    },
+    payload,
+    {
+      new: true,
+      includeResultMetadata: true,
+      ...options,
+    },
+  );
+  if (!rawResult || !rawResult.value) return null;
+
+  return {
+    student: rawResult.value,
+    isNew: Boolean(rawResult?.lastErrorObject?.upserted),
+  };
+};
+
+// Опція includeResultMetadata визначає тип повернення складених методів.
+
+// Цей параметр за замовчуванням має значення false, що означає, що кожен метод повертає знайдений документ. Якщо документ не знайдено, кожен метод повертає значення null. Якщо встановити includeResultMetadata на true, метод повертає тип ModifyResult, який містить знайдений документ і метадані.
+
+// Вбудований документ lastErrorObject містить такі поля:
+
+// n (ціле число) – містить кількість документів, що відповідають предикату оновлення, або кількість документів, які команда вставила або видалила.
+
+// updatedExisting (логічне значення) – Містить значення true, якщо операція оновлення:
+// - оновила існуючий документ.
+// - документ знайдено, але він вже перебував у бажаному стані призначення, тому оновлення фактично не відбулося.
+
+// upserted (документ) – Містить ObjectId вставленого документа, якщо операція оновлення з upsert: true призвела до створення нового документа.
